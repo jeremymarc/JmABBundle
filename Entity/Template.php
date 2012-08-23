5f2b841cce02a4bf8c0b5c009acff2687af935be
Jeremy Marc <jeremy.marc@me.com>
2012-08-23 14:19:41 -0700

    [Doctrine] Add index and unique annotation on name

--- Entity/Template.php
+++ Entity/Template.php
@@ -8,196 +8,196 @@ use Gedmo\Mapping\Annotation as Gedmo;
 /**
  * Jm\ABBundle\Entity\Template
  *
- * @ORM\Table(name="template")
+ * @ORM\Table(name="template",indexes={@index(name="search_name", columns={"name"})})
  * @ORM\Entity(repositoryClass="Jm\ABBundle\Entity\TemplateRepository")
  * @ORM\HasLifecycleCallbacks
  */
 class Template
 {
-	/**
-	 * @var integer $id
-	 *
-	 * @ORM\Column(name="id", type="integer")
-	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="AUTO")
-	 */
-	private $id;
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
-	/**
-	 * @var string $name
-	 *
-	 * @ORM\Column(name="name", type="string", length=255)
-	 */
-	private $name;
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", unique=true, type="string", length=255)
+     */
+    private $name;
 
-	/**
-	 * @var string $body
-	 *
-	 * @ORM\Column(name="body", type="text")
-	 */
-	private $body;
+    /**
+     * @var string $body
+     *
+     * @ORM\Column(name="body", type="text")
+     */
+    private $body;
 
-	/**
-	 *
-	 * @ORM\Column(name="variation", type="text")
-	 */
-	private $variationBody;
+    /**
+     *
+     * @ORM\Column(name="variation", type="text", nullable=true)
+     */
+    private $variationBody;
 
-	/**
-	 * @var \DateTime $updateTime
-	 *
-	 * @ORM\Column(name="updateTime", type="datetime", nullable=true)
-	 */
-	private $updateTime;
+    /**
+     * @var \DateTime $updateTime
+     *
+     * @ORM\Column(name="updateTime", type="datetime", nullable=true)
+     */
+    private $updateTime;
 
-	/**
-	 * @var \DateTime $creationTime
-	 *
-	 * @ORM\Column(name="creationTime", type="datetime")
-	 */
-	private $creationTime;
+    /**
+     * @var \DateTime $creationTime
+     *
+     * @ORM\Column(name="creationTime", type="datetime")
+     */
+    private $creationTime;
 
-	/**
-	 * Get id
-	 *
-	 * @return integer
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * Set name
-	 *
-	 * @param string $name
-	 * @return Page
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Page
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get name
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * Set body
-	 *
-	 * @param string $body
-	 * @return Page
-	 */
-	public function setBody($body)
-	{
-		$this->body = $body;
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Page
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get body
-	 *
-	 * @return string
-	 */
-	public function getBody()
-	{
-		return $this->body;
-	}
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
 
-	/**
-	 * Get variationBody.
-	 *
-	 * @return variationBody.
-	 */
-	public function getVariationBody()
-	{
-		return $this->variationBody;
-	}
+    /**
+     * Get variationBody.
+     *
+     * @return variationBody.
+     */
+    public function getVariationBody()
+    {
+        return $this->variationBody;
+    }
 
-	/**
-	 * Set variationBody.
-	 *
-	 * @param variationBody the value to set.
-	 */
-	public function setVariationBody($variationBody)
-	{
-		$this->variationBody = $variationBody;
-	}
+    /**
+     * Set variationBody.
+     *
+     * @param variationBody the value to set.
+     */
+    public function setVariationBody($variationBody)
+    {
+        $this->variationBody = $variationBody;
+    }
 
-	/**
-	 * Set creationTime
-	 *
-	 * @param \DateTime $creationTime
-	 * @return Page
-	 */
-	public function setCreationTime($creationTime)
-	{
-		$this->creationTime = $creationTime;
+    /**
+     * Set creationTime
+     *
+     * @param \DateTime $creationTime
+     * @return Page
+     */
+    public function setCreationTime($creationTime)
+    {
+        $this->creationTime = $creationTime;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get creationTime
-	 *
-	 * @return \DateTime
-	 */
-	public function getCreationTime()
-	{
-		return $this->creationTime;
-	}
+    /**
+     * Get creationTime
+     *
+     * @return \DateTime
+     */
+    public function getCreationTime()
+    {
+        return $this->creationTime;
+    }
 
-	/**
-	 * Set updateTime
-	 *
-	 * @param \DateTime $updateTime
-	 * @return Page
-	 */
-	public function setUpdateTime($updateTime)
-	{
-		$this->updateTime = $updateTime;
+    /**
+     * Set updateTime
+     *
+     * @param \DateTime $updateTime
+     * @return Page
+     */
+    public function setUpdateTime($updateTime)
+    {
+        $this->updateTime = $updateTime;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get updateTime
-	 *
-	 * @return \DateTime
-	 */
-	public function getUpdateTime()
-	{
-		return $this->updateTime;
-	}
+    /**
+     * Get updateTime
+     *
+     * @return \DateTime
+     */
+    public function getUpdateTime()
+    {
+        return $this->updateTime;
+    }
 
-	public function __toString()
-	{
-		return $this->getName();
-	}
+    public function __toString()
+    {
+        return $this->getName();
+    }
 
-	/**
-	 * @ORM\PrePersist
-	 */
-	public function beforePersist()
-	{
-		$this->setCreationTime(new \DateTime());
-		$this->setUpdateTime(new \DateTime());
-	}
+    /**
+     * @ORM\PrePersist
+     */
+    public function beforePersist()
+    {
+        $this->setCreationTime(new \DateTime());
+        $this->setUpdateTime(new \DateTime());
+    }
 
-	/**
-	 * @ORM\PreUpdate
-	 */
-	public function beforeUpdate()
-	{
-		$this->setUpdateTime(new \DateTime());
-	}
+    /**
+     * @ORM\PreUpdate
+     */
+    public function beforeUpdate()
+    {
+        $this->setUpdateTime(new \DateTime());
+    }
 }
