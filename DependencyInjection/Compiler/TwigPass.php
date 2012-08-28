a08fe0980d38be2698a8274c84930bd8c21805d6
Jeremy Marc <jeremy.marc@me.com>
2012-08-28 10:36:15 -0700

    [config] Make the loader working with existing Twig_Chain_Loader

--- DependencyInjection/Compiler/TwigPass.php
+++ DependencyInjection/Compiler/TwigPass.php
@@ -9,9 +9,21 @@ class TwigPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $abTwigLoader = $container->getDefinition('jm_ab.twig_loader');
-
         if (true === $container->getParameter('jm_ab.custom_loader')) {
+            $loader = $container->getDefinition('twig.loader');
+
+            $class = $loader->getClass();
+            if (preg_match("/%(.*)%/", $class, $m)) {
+                $class = $container->getParameter($m[1]);
+            }
+
+            if ("Twig\Loader\Chain" === $class) {
+                $loader->addMethodCall('addLoader', array($container->getDefinition('jm_ab.template_loader')));
+                return;
+            }
+
+            $abTwigLoader = $container->getDefinition('jm_ab.twig_loader');
+            $abTwigLoader->addMethodCall('addLoader', array($loader));
             $container->setDefinition('twig.loader', $abTwigLoader);
         }
     }
